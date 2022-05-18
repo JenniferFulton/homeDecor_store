8062994b9613e7c61d8025ba04a8a16128a3cec9
JenniferFulton <jennifer.reeves8@gmail.com>
2022-05-18 19:32:21 -0400

    validators created for item and warehouse models

--- homeDecor_store/store/models.py
+++ homeDecor_store/store/models.py
@@ -1,5 +1,52 @@
 from django.db import models
 
+class WarehouseManager(models.Manager):
+    def newWarehouse_validator(self, postData):
+        # validates a warehouse that is being added by a user
+        errors = {}
+
+        if len(postData['name']) <= 3:
+            errors['name'] = 'Warehouse nickname must be atleast 3 characters'
+        
+        if postData['address'].isnumeric() == True or postData['address'].isalpha() == True:
+            errors['address'] = 'Please include building number and street name in your address'
+        
+        if len(postData['city']) <= 2:
+            errors['city'] = "Please include valid city"
+        
+        if len(postData['state']) != 2:
+            errors['state'] = "Please use state abbreviation EX: MD for Maryland"
+        
+        if postData['zip_code'].isnumeric() == False or len(postData['zip_code']) != 5:
+            errors['zip_code'] = 'Please use 5 numbers'
+        
+        return errors
+
+class ItemManager(models.Manager):
+    def newItem_validator(self, postData):
+        # validates an item that is being added by a user
+        errors = {}
+
+        if len(postData['name']) <= 2:
+            errors['name'] = 'Item name must be atleast 3 characters'
+        
+        if len(postData['description']) <= 5:
+            errors['description'] = "Items's description must have more than 10 characters"
+        
+        if postData['height'].isnumeric() == True:
+            errors['height'] = 'Please include number and units in your height dementions, Ex: 8" or 8 inches'
+        
+        if postData['width'].isnumeric() == True:
+            errors['width'] = 'Please include number and units in your width dementions, Ex: 8" or 8 inches'
+        
+        if len(postData['color']) <= 2:
+            errors['color'] = 'Color must be atleast 3 characters'
+        
+        if postData['quantity'].isnumeric() == False:
+            errors['quantity'] = 'Please use numbers only for quantity'
+        
+        return errors
+
 class Warehouse(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
@@ -8,6 +55,7 @@ class Warehouse(models.Model):
     zip_code = models.CharField(max_length=5)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    objects = WarehouseManager()
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
@@ -20,3 +68,4 @@ class Item(models.Model):
     location = models.ManyToManyField(Warehouse,related_name="warehouse")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    objects = ItemManager()
