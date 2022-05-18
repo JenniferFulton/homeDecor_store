b89dced1dd6dc4356b7b7c5cdf010588d8fb3913
JenniferFulton <jennifer.reeves8@gmail.com>
2022-05-18 17:17:38 -0400

    HTML formatting updated. Not updating the editting of an item successfully

--- homeDecor_store/store/urls.py
+++ homeDecor_store/store/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('warehouse_success', views.warehouse_success),
     path('item_success', views.item_success),
     path('edit_item/<int:id>', views.edit_item),
-    # path('edit_success', views.edit_success),
+    path('edit_success/<int:id>', views.edit_success),
+    path('delete_item', views.delete_item),
 ]
--- homeDecor_store/store/views.py
+++ homeDecor_store/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import *
 
 def home_page(request):
+    #home page displays inventory with option to add new item to inventory
     all_items = Item.objects.all()
     context = {
         'inventory' : all_items
@@ -9,6 +10,8 @@ def home_page(request):
     return render(request, 'home.html', context)
 
 def new_item(request):
+    #brings user to a page where there is a form to add new item and 
+    #option to add a new warehouse
     all_warehouses = Warehouse.objects.all()
     context = {
         'locations' : all_warehouses
@@ -16,6 +19,8 @@ def new_item(request):
     return render(request, 'add_item.html', context)
 
 def new_warehouse(request):
+    #brings user to a page with a form to add a new warehouse location and
+    #show current warehouse locations
     all_warehouses = Warehouse.objects.all()
     context = {
         'locations' : all_warehouses
@@ -23,6 +28,7 @@ def new_warehouse(request):
     return render(request, 'add_warehouse.html', context)
 
 def warehouse_success(request):
+    #adds a new warehouse and goes back to warehouse page
     if request.method == 'POST':
         Warehouse.objects.create(
         name = request.POST['name'],
@@ -33,9 +39,8 @@ def warehouse_success(request):
             )
     return redirect('/new_warehouse')
 
-#edit a location
-
 def item_success(request):
+    #adds a new item to inventory
     if request.method == 'POST':
         Item.objects.create(
         name = request.POST['name'],
@@ -53,6 +58,7 @@ def item_success(request):
         return redirect('/new_item')
 
 def edit_item(request,id):
+    #brings user to a page with a form to edit a specific item
     current_item = Item.objects.get(id=id)
     all_warehouses = Warehouse.objects.all()
     context = {
@@ -61,7 +67,23 @@ def edit_item(request,id):
     }
     return render(request, 'edit_item.html', context)
 
-# def edit_success(request):
+def edit_success(request,id):
+    #updates item information
+    to_edit = Item.objects.get(id=id)
+    if request.method == 'POST':
+        to_edit.name = request.POST['new_name'],
+        to_edit.description = request.POST['new_description'],
+        to_edit.height = request.POST['new_height'],
+        to_edit.width = request.POST['new_width'],
+        to_edit.color = request.POST['new_color'],
+        to_edit.quantity = request.POST['new_quantity'],
+        
+        location = Warehouse.objects.get(id = request.POST['new_location'])
+        location.warehouse.add(to_edit)
+
+        to_edit.save()
+    
+    return redirect('/')
 
 
     
